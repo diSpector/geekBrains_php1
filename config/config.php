a826da44eed10537b330f0cf0cb37979ac1d8f86
dimudila <dmitry_proekt@mail.ru>
2018-11-11 04:47:44 +0300

    Удалены файлы ДЗ3, добавлены файлы ДЗ4

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+define('SITE_ROOT', "../");
+
+define('WWW_ROOT', SITE_ROOT . 'public/');
+define('IMG_DIR', WWW_ROOT . 'img');
+define('IMAGES', './public/img');
+
+define('DATA_DIR', SITE_ROOT . '/data');
+define('LIB_DIR', SITE_ROOT . '/engine');
+define('TPL_DIR', SITE_ROOT . '/templates');
+
+define('SITE_TITLE', 'Урок 4');
+
+require_once(LIB_DIR . '/functions.php');
+?>
